6b96229629a73e11e558db21eb06a3e1a159f57c
mingwei.shao <13380053360@163.com>
2025-04-30 11:16:37 +0800

    yyyyy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+        '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
 })
