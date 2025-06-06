a397fe281ce03ba67887cd5d65c75203ce523c1b
mingwei.shao <13380053360@163.com>
2025-06-07 00:11:11 +0800

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 修改为您的 GitHub 仓库名
+  base: '/vip/', // 修改为您的 GitHub 仓库名
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 }) 
