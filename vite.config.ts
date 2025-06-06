42535271b82511ad6725405fef4b2a852d931f53
mingwei.shao <13380053360@163.com>
2025-06-07 00:00:28 +0800

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dianyi-cloud/', // 修改为您的 GitHub 仓库名
+  base: '/', // 修改为您的 GitHub 仓库名
   plugins: [vue()],
 }) 
