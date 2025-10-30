451eb3b540c67661348fba6d33e6cea060d696eb
mingwei.shao <standardsoftware@M4-PRO.local>
2025-10-30 13:50:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // GitHub Pages 项目页路径
-  base: '/vip/',
+  base: '/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
